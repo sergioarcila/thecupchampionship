31f9d1e1d04025361026ed24b3529dfef8654e89
sergioarcila <sergio.arcila082@gmail.com>
2020-04-12 21:27:16 -0400

    Disable send email function:crashing app

--- server/mailURL.js
+++ server/mailURL.js
@@ -1,1 +1,1 @@
-process.env.MAIL_URL = "smtp://postmaster@thecupchampionship.com:386f54d01a8bbb8db6508e10b24b3c20@smtp.mailgun.org:587";
+//process.env.MAIL_URL = "smtp://postmaster@thecupchampionship.com:386f54d01a8bbb8db6508e10b24b3c20@smtp.mailgun.org:587";
